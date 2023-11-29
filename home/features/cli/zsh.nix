25f7e97530bae88d6aa1ac68e634ce2168d9bb47
ben <benbaldwin000@gmail.com>
2023-11-29 14:51:19 -0700

    Wed

--- home/features/cli/zsh.nix
+++ home/features/cli/zsh.nix
@@ -5,8 +5,14 @@
     enableAutosuggestions = true;
     syntaxHighlighting.enable = true;
     shellAliases = {
-      switch-sys = "sudo nixos-rebuild switch --flake ~/.flake";
-      switch-home = "home-manager switch --flake ~/.flake";
+      pre-switch = ''
+        cd ~/.flake &&
+        nix flake lock &&
+        git add . &&
+        git commit -m "$(date)"
+      '';
+      switch-sys = ''pre-switch && sudo nixos-rebuild switch --flake ~/.flake'';
+      switch-home = ''pre-switch && home-manager switch --flake ~/.flake'';
     };
   };
 }
